3190b7678bfeb1d54f26163da8127cd5279e5b93
mid4s <mid4s@localhost.localdomain>
2025-11-03 00:11:19 +0300

    Rebuilt Django project with bookshelf app and Book model

--- Introduction_to_Django/LibraryProject/LibraryProject/settings.py
+++ Introduction_to_Django/LibraryProject/LibraryProject/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-hl3+3!u8&w9z89oc%p$0qm#ixwk(j7&y=6u@f(b6)!@l2tva^p'
+SECRET_KEY = 'django-insecure-9a!a)u@=b)qz8z179r$8zgu!6^lyl2v9a&v!3z-aa==)dzdj#6'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
--- Introduction_to_Django/LibraryProject/retrieve.md
+++
@@ -1,7 +0,0 @@
-from bookshelf.models import Book
-
-# Retrieve and display all attributes of the created book
-book = Book.objects.get(title="1984")
-book.title, book.author, book.publication_year
-# Expected Output:
-# ('1984', 'George Orwell', 1949)
